5a98239d5fb832f008bd4dac5a3832da65ac84a1
feven <feventsegay6@gmail.com>
2019-04-20 18:28:05 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,17 @@
-def hello_t
-
+def hello_t(array)
+ array.each do |name|
+   puts "#{name}"
+   yield
+   end
 end
-
 # call your method here!
+hello_t(["Tim", "Tom", "Jim"])
+hello_t(["Ali", "Jasmine", "Persephone"])do |name|
+  puts "#{name.upcase}"
+end
+  
+
+
+
+
 
