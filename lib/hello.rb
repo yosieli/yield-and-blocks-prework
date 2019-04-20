a814a89dcd411b1332ff37a8f740a02436f48d45
feven <feventsegay6@gmail.com>
2019-04-20 18:27:52 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,13 @@
 def hello_t(array)
  array.each do |name|
    puts "#{name}"
+   yield
    end
 end
 # call your method here!
 hello_t(["Tim", "Tom", "Jim"])
-hello_t(["ALI", "JASMINE", "PERSEPHONE"])
+hello_t(["Ali", "Jasmine", "Persephone"])do |name|
+  puts "#{name.upcase}"
 end
   
 
